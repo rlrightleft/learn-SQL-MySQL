60c2d5ce460430867f2fa88a5b88046eb3089f16
R L <la.didipierce@gmail.com>
2025-03-02 14:36:56 -0500

    Update 4_SELECT_within_SELECT.sql

--- 4_SELECT_within_SELECT.sql
+++ 4_SELECT_within_SELECT.sql
@@ -62,7 +62,7 @@ Using two subqueries ensures the population falls within a defined range.
 */
 
 -- Problem 5: Germany (population roughly 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany. Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
---| Name      | Percentage |
+--| Name      | Percentage|
 --|-----------|-----------|
 --| Albania   | 3%        |
 --| Andorra   | 0%        |
