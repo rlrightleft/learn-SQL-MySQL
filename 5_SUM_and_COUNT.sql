ae1dc9bc5477eb63674118eeda8d45682cf70d52
R L <191174016+rlrightleft@users.noreply.github.com>
2025-03-04 20:52:52 -0500

    Update 5_SUM_and_COUNT.sql

--- 5_SUM_and_COUNT.sql
+++ 5_SUM_and_COUNT.sql
@@ -76,3 +76,80 @@ HAVING SUM(population) >= 100000000;
 The **HAVING** clause allows us to filter the GROUP BY groups which are displayed. 
 The WHERE clause filters rows before the aggregation, the **HAVING** clause filters after the aggregation.
 */
+
+
+-- Additional problems related to the nobel table:
+-- The nobel table can be used to practice more SUM and COUNT functions.
+-- Nobel Prizes: Aggregate functions
+-- This tutorial concerns aggregate functions such as COUNT, SUM and AVG.
+
+-- Nobel Problem 1: Show the total number of prizes awarded.
+SELECT COUNT(winner) 
+FROM nobel;
+
+-- Nobel Problem 2: List each subject - just once
+SELECT DISTINCT subject
+FROM nobel;
+
+-- Nobel Problem 3: Show the total number of prizes awarded for Physics.
+SELECT COUNT(winner)
+FROM nobel
+WHERE subject = 'Physics';
+
+-- Nobel Problem 4: For each subject show the subject and the number of prizes.
+SELECT subject, COUNT(winner)
+FROM nobel
+GROUP BY subject;
+
+-- Nobel Problem 5: For each subject show the first year that the prize was awarded.
+SELECT subject, yr
+FROM nobel x
+WHERE yr <= 
+  ALL(
+    SELECT yr
+    FROM nobel y
+    WHERE y.subject = x.subject
+  )
+GROUP BY subject;
+
+-- Nobel Problem 6: For each subject show the number of prizes awarded in the year 2000.
+SELECT subject, COUNT(winner)
+FROM nobel
+WHERE yr = 2000
+GROUP BY subject;
+
+-- Nobel Problem 7: Show the number of different winners for each subject. Be aware that Frederick Sanger has won the chemistry prize twice - he should only be counted once.
+SELECT subject, COUNT(DISTINCT winner)
+FROM nobel
+GROUP BY subject;
+
+-- Nobel Problem 8: For each subject show how many years have had prizes awarded.
+SELECT subject, COUNT(DISTINCT yr)
+FROM nobel
+GROUP BY subject;
+
+-- Nobel Problem 9: Show the years in which three prizes were given for Physics.
+SELECT yr
+FROM nobel
+WHERE subject = 'Physics'
+GROUP BY yr
+HAVING COUNT(winner) = 3;
+
+-- Nobel Problem 10: Show winners who have won more than once.
+SELECT winner
+FROM nobel
+GROUP BY winner
+HAVING count(winner) > 1;
+
+-- Nobel Problem 11: Show winners who have won more than one subject.
+SELECT winner
+FROM nobel
+GROUP BY winner
+HAVING COUNT(DISTINCT subject) > 1;
+
+-- Nobel Problem 12: Show the year and subject where 3 prizes were given. Show only years 2000 onwards.
+SELECT yr, subject
+FROM nobel
+WHERE yr >= 2000
+GROUP BY yr, subject
+HAVING COUNT(winner) = 3;
