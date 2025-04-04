612588c09405149387fe6899423d5b49b23f9d4f
R L <191174016+rlrightleft@users.noreply.github.com>
2025-04-03 20:38:48 -0400

    Update 10_TUTORIAL_QUIZZES.sql

--- sqlzoo/10_TUTORIAL_QUIZZES.sql
+++ sqlzoo/10_TUTORIAL_QUIZZES.sql
@@ -142,4 +142,65 @@ SELECT name FROM world
 
 -- Quiz 3: Nobel Quiz
 
--- Question 3.1:
+-- Question 3.1: Pick the code which shows the name of winner's names beginning with C and ending in n
+SELECT winner 
+FROM nobel
+WHERE winner LIKE 'C%' 
+AND winner LIKE '%n';
+
+-- Question 3.2: Select the code that shows how many Chemistry awards were given between 1950 and 1960
+SELECT COUNT(subject) 
+FROM nobel
+WHERE subject = 'Chemistry'
+AND yr BETWEEN 1950 and 1960;
+
+-- Question 3.3: Pick the code that shows the amount of years where no Medicine awards were given
+SELECT COUNT(DISTINCT yr) 
+FROM nobel
+WHERE yr NOT IN 
+  (SELECT DISTINCT yr 
+  FROM nobel 
+  WHERE subject = 'Medicine');
+
+-- Question 3.4: Select the result that would be obtained from the following code:
+SELECT subject, winner FROM nobel WHERE winner LIKE 'Sir%' AND yr LIKE '196%';
+-- Answer 3.4: 
+  -- Medicine	| Sir John Eccles
+  -- Medicine	| Sir Frank Macfarlane Burnet
+
+-- Question 3.5: Select the code which would show the year when neither a Physics or Chemistry award was given
+SELECT yr 
+FROM nobel
+WHERE yr NOT IN
+  (SELECT yr 
+  FROM nobel
+  WHERE subject IN ('Chemistry','Physics'));
+
+-- Question 3.6: Select the code which shows the years when a Medicine award was given but no Peace or Literature award was
+SELECT DISTINCT yr
+FROM nobel
+WHERE subject='Medicine' 
+AND yr NOT IN
+  (SELECT yr 
+  FROM nobel 
+  WHERE subject='Literature')
+AND yr NOT IN 
+  (SELECT yr 
+  FROM nobel
+  WHERE subject='Peace');
+
+-- Question 3.7: Pick the result that would be obtained from the following code:
+SELECT subject, COUNT(subject) 
+   FROM nobel 
+  WHERE yr ='1960' 
+  GROUP BY subject;
+-- Answer 3.7: 
+  -- Chemistry  |	1
+  -- Literature	| 1
+  -- Medicine	  | 2
+  -- Peace	    | 1
+  -- Physics	  | 1
+
+-- Quiz 4: Nested SELECT Quiz
+
+-- Question 4.1:
